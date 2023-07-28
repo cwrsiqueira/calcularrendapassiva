d8840f5ddbeab058f13dd97e3b155ed54513402e
cwrsiqueira <cwrsiqueira@msn.com>
2023-07-28 07:43:36 -0300

    Configuração Inicial da Calculadora

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,10 @@ export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+}) {  
   return (
     <html lang="en">
-      <body className='font-sans text-stone-950'>
-        <nav className="flex justify-between bg-stone-400 p-2">
-          <div className='uppercase font-semibold'><a href="/">Calculadora de Renda Passiva</a></div>
-          <ul className='flex justify-between'>
-            <li className='p-1 border-2 border-stone-400 hover:text-white hover:border-white'><a href="/">Home</a></li>
-            <li className='p-1 border-2 border-stone-400 hover:text-white hover:border-white'><a href="/sobre">Sobre</a></li>
-            <li className='p-1 border-2 border-stone-400 hover:text-white hover:border-white'><a href="/contato">Contato</a></li>
-            <li className='p-1 border-2 border-stone-400 hover:text-white hover:border-white'><a href="/doacoes">Doações</a></li>
-          </ul>
-        </nav>
+      <body className='font-sans text-stone-700'>
         <main>{children}</main>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,187 @@
+'use client'
+import { MouseEvent, useEffect, useState } from "react"
+
 export default function Home() {
+  const [periodo, setPeriodo] = useState('')
+  const [vlrIni, setVlrIni] = useState('')
+  const [vlrRecorr, setVlrRecorr] = useState('')
+  const [txPeriodo, setTxPeriodo] = useState('')
+  const [renda, setRenda] = useState('')
+
+  const menus = [
+    {
+      title: 'Home',
+      url: '/'
+    },
+    {
+      title: 'Doações',
+      url: '/#doacoes'
+    },
+  ]
+
+  function maskMoney(str: string) {
+    let parte1;
+    let parte2;
+    let parte11;
+    let parte12;
+    let parte111;
+    let parte112;
+
+    str = str.replace(',', '')
+
+    if(str.length == 0){
+      return str;
+    }
+
+    parte1 = str.slice(0, -2);
+    parte2 = str.slice(-2);
+
+    if (parte1.length > 3) {
+      parte11 = parte1.slice(0, -3)
+      parte12 = parte1.slice(-3)
+
+      if (parte11.length > 3) {
+        parte111 = parte11.slice(0, -3)
+        parte112 = parte11.slice(-3)
+
+        parte11 = `${parte111}.${parte112}`
+      }
+
+      parte1 = `${parte11}.${parte12}`
+
+    }
+
+    return `${parte1},${parte2}`;
+  }
+
+  function formatarVlr(vlr: string, campo: string) {
+
+    if (vlr.length > 14) {
+      return false;
+    }
+    let valorFormatado = '';
+    let newValor = vlr.replace(/[^\d,]/g, '');
+
+    valorFormatado = maskMoney(newValor)
+
+    setVlrIni(valorFormatado);
+
+
+
+    // if(vlr.length < 1){
+    //   vlr = '0'
+    // }
+
+    // if(vlr.substring(0, 1) == '0' && vlr.length > 1){
+    //   vlr = vlr.substring(1)
+    // }
+
+    // // Remove tudo que não for dígito, ponto ou vírgula
+    // const valorSemFormatacao = vlr.replace(/[^\d.,]/g, '');
+
+    // // Substitui a vírgula pela base usada no parseFloat (ponto) se houver
+    // const valorComPonto = valorSemFormatacao.replace(',', '.');
+
+    // // Formata o valor como no exemplo anterior
+    // const numeroFormatado = parseFloat(valorComPonto).toFixed(2);
+    // const [parteInteira, parteDecimal] = numeroFormatado.split('.');
+    // const parteInteiraFormatada = parteInteira.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    // const valorFormatado = `${parteInteiraFormatada},${parteDecimal}`;
+    // console.log(valorFormatado)
+
+    // // Atualiza o valor do input com o valor formatado
+    // if(campo == 'setVlrIni'){
+    //   setVlrIni(vlr)
+    // }
+  }
+
+  const handleCalc = (e: MouseEvent<HTMLInputElement>) => {
+    e.preventDefault()
+
+    let camposPreenchidos = [
+      periodo,
+      vlrIni,
+      vlrRecorr,
+      txPeriodo,
+      renda,
+    ]
+
+    let totalCamposNPreenchidos = 0;
+    let campoEmBranco: number = 999;
+    camposPreenchidos.forEach((item, key) => {
+      if (item == '') {
+        totalCamposNPreenchidos++;
+        campoEmBranco = key
+      }
+    })
+
+    if (totalCamposNPreenchidos > 1) {
+      alert('Erro! Apenas 1 campo deve ser deixado em branco.')
+      return false;
+    }
+
+    if (totalCamposNPreenchidos < 1) {
+      alert('Erro! Pelo menos 1 campo deve ser deixado em branco.')
+      return false;
+    }
+
+    switch (campoEmBranco) {
+      case 0:
+        calcularTempoAplicacao
+        break;
+      case 1:
+        calcularValorInicial
+        break;
+      case 2:
+        calcularValorRecorrente
+        break;
+      case 3:
+        calcularTaxaMensal
+        break;
+      case 4:
+        calcularRendaPassiva
+        break;
+
+      default:
+        alert('Erro! Campo em branco não identificado. Tente novamente.')
+        break;
+    }
+  }
+
+  const calcularTempoAplicacao = () => { }
+  const calcularValorInicial = () => { }
+  const calcularValorRecorrente = () => { }
+  const calcularTaxaMensal = () => { }
+  const calcularRendaPassiva = () => { }
+
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <nav className="flex justify-between bg-stone-400 p-2">
+        <div className='flex items-center uppercase font-semibold'><a href="/">Calculadora de Renda Passiva</a></div>
+        <ul className='flex justify-between'>
+          {menus.map((item, index) => (
+            <li key={index} className={`p-1 border-2 border-stone-400 hover:text-white hover:border-white`}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main className="flex justify-center mt-1">
+        <div className="flex justify-center rounded shadow-lg md:w-1/2">
+          <div className="flex justify-center flex-col items-center p-3">
+            <h1 className="uppercase font-semibold text-slate-500">Calculadora</h1>
+            <small>Deixe em branco o campo que você quer calcular</small>
+            <form>
+              <input className="w-full my-2 p-2 outline-0 border-indigo-500 focus:border-b-2 ease-in-out duration-150" type="number" name="periodo" id="periodo" value={periodo} onChange={(e) => setPeriodo(e.target.value)} placeholder="Tempo de aplicação em meses" />
+              <input className="w-full my-2 p-2 w-full my-2 p-2 focus:outline-0 focus:border-b-2 focus:border-indigo-500 ease-in-out duration-150" type="text" name="vlrIni" id="vlrIni" value={vlrIni} onChange={(e) => formatarVlr(e.target.value, 'setVlrIni')} placeholder="Valor inicial" />
+              <input className="w-full my-2 p-2 w-full my-2 p-2 focus:outline-0 focus:border-b-2 focus:border-indigo-500 ease-in-out duration-150" type="text" name="vlrRecorr" id="vlrRecorr" value={vlrRecorr} onChange={(e) => setVlrRecorr(e.target.value)} placeholder="Valor recorrente" />
+              <input className="w-full my-2 p-2 w-full my-2 p-2 focus:outline-0 focus:border-b-2 focus:border-indigo-500 ease-in-out duration-150" type="text" name="txPeriodo" id="txPeriodo" value={txPeriodo} onChange={(e) => setTxPeriodo(e.target.value)} placeholder="Taxa mensal" />
+              <input className="w-full my-2 p-2 w-full my-2 p-2 focus:outline-0 focus:border-b-2 focus:border-indigo-500 ease-in-out duration-150" type="text" name="renda" id="renda" value={renda} onChange={(e) => setRenda(e.target.value)} placeholder="Renda passiva" />
+              <input className=" cursor-pointer uppercase w-full my-2 p-2 bg-blue-700 rounded text-white hover:bg-blue-600 font-medium" type="submit" onClick={(e) => handleCalc(e)} value="Calcular" />
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
