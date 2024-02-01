a72ff94bef507ea12b8420441f11b4be7aab51cf
cwrsiqueira <cwrsiqueira@msn.com>
2024-02-01 03:04:40 -0300

    Inclusão do botão Obter no Google Play e melhorado o botão de patrocínio

--- app/page.tsx
+++ app/page.tsx
@@ -467,13 +467,27 @@ const Home = () => {
           </div>
 
           <div className="flex flex-col items-center text-center mt-3">
-            <button className="border border-blue-300 p-3 rounded bg-blue-300">
+
+            <div className="text-center sm:w-80 p-4 mt-4 font-semibold">Considere ser um patrocinador, por apenas R$ 4,99 por mês e me ajude e manter a Calculadora de Renda Passiva sempre GRATUITA!</div><hr />
+
+            <button className="border border-slate-600 p-4 rounded-md bg-slate-900 text-white text-2xl font-semibold">
               <FontAwesomeIcon icon={faHeart} beat style={{ color: "#b40000", }} />
               <a href="https://www.mercadopago.com.br/subscriptions/checkout?preapproval_plan_id=2c938084882ea1dd0188305e8e0e00bc" target="_blank">&nbsp; Seja um Patrocinador</a>
             </button>
-            <div className="text-center sm:w-80 p-4 mt-4">Considere ser um patrocinador, por apenas R$ 4,99 por mês e me ajude e manter a Calculadora de Renda Passiva sempre GRATUITA!</div>
-            <div className="text-center sm:w-80 p-4 mt-4">
-              Conheça meus outros projetos nas minhas redes <br /> <br />
+
+            <div className="text-center sm:w-80 p-4 mt-4 font-semibold">
+              Baixe o App da Calculadora de Renda Passiva e tenha a calculadora a mão, mesmo sem internet. <br /><hr />
+              <div>
+                <a href='https://play.google.com/store/apps/details?id=com.cwrsiqueira.crp_app&pcampaignid=web_share&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1' target="_blank">
+                  <img alt='Disponível no Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/pt_badge_web_generic.png' />
+                </a>
+              </div>
+            </div>
+
+
+            <div className="text-center sm:w-80 p-4 mt-4 font-semibold">
+              Conheça meus outros projetos nas minhas redes <br /><hr className="mb-2" />
+
               <div className="flex justify-center">
                 <a href="https://github.com/cwrsiqueira?tab=repositories" target="_blank" className="mx-3">
                   <FontAwesomeIcon icon={faGithub} size="xl" />
@@ -488,6 +502,7 @@ const Home = () => {
                   <FontAwesomeIcon icon={faEnvelope} size="xl" />
                 </a>
               </div>
+
             </div>
           </div>
         </div>
